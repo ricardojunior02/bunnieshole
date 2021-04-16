ef89be3346a85c30f0a43d054c624e485be7459e
ricardojunior02 <ricardojunior199502@gmail.com>
2021-04-16 10:07:29 -0300

    Componente de aviso

---
+++ frontend/src/components/Warning/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container, Content } from './styles';
+
+function Warning() {
+  return (
+    <Container>
+      <Content>
+        <h1>Desculpe não encontramos nenhum produto...</h1>
+      </Content>
+    </Container>
+  )
+}
+
+export default Warning;
---
+++ frontend/src/components/Warning/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Content = styled.div`
+    max-width: 1440px;
+    margin: 0 auto;
+
+    h1 {
+      font-weight: 500;
+      line-height: 75px;
+    }
+`;
